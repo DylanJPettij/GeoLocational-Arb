893789ebf7f833a1a487af90e82ebac57f553f00
DylanJPettij <45019470+DylanJPettij@users.noreply.github.com>
2020-10-01 15:40:36 -0400

    Add files via upload
    Adding the initial files and project

---
+++ ArbProject.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator) 
+CostOfLiving = pd.read_excel (r'C:\Users\Dylan\PythonAnalysis\Cost Of Living Index.xlsx')
+del CostOfLiving['Rank']
+
+def mergeData():
+    City_Living_Rent_LocalPurchasingPower = CostOfLiving[['City','Cost of Living Plus Rent Index', 'Local Purchasing Power Index']]
+    sorted_df = City_Living_Rent_LocalPurchasingPower.sort_values('Cost of Living Plus Rent Index', ascending = True)
+    return(sorted_df)
+
+def FixData(sorted_df):
+    CostOfLiving = sorted_df.sort_values('Cost of Living Plus Rent Index', ascending = True)
+    BelowAverageCost = CostOfLiving[CostOfLiving['Cost of Living Plus Rent Index'] < CostOfLiving['Cost of Living Plus Rent Index'].mean()] 
+    AboveAveragePurchasingPower = CostOfLiving[CostOfLiving['Local Purchasing Power Index'] > CostOfLiving['Local Purchasing Power Index'].mean()] 
+    AboveAveragePurchasingPower = AboveAveragePurchasingPower.sort_values('Local Purchasing Power Index', ascending = False)
+    BelowAverageCost = BelowAverageCost[BelowAverageCost['Cost of Living Plus Rent Index'] >=19] #setting a floor for cost of living
+    BelowAverageCost= BelowAverageCost.reset_index()
+    AboveAveragePurchasingPower= AboveAveragePurchasingPower.reset_index()
+    del BelowAverageCost['index']
+    del AboveAveragePurchasingPower['index']
+    return BelowAverageCost, AboveAveragePurchasingPower
+
+def Charting(Cost, PurchasingPower):
+    C10 =Cost.head(30)
+    C10['Cost of Living Plus Rent Index']=100*(C10['Cost of Living Plus Rent Index']/C10['Cost of Living Plus Rent Index'].mean()-1)
+    x=C10['City']
+    y=C10['Cost of Living Plus Rent Index']
+    #charting portion
+    colormat=np.where(C10['Cost of Living Plus Rent Index']>0, 'g','r')
+    plt.title("Top 10 Places with the Lowest Cost of Living Index Including Rent")
+    plt.xlabel("% Disparity")
+    plt.ylabel("Cities")
+    plt.barh(x,y,color =colormat, edgecolor="black")
+    plt.show()
+    plt.close()
+    print("-------------------------")
+    PurchasingPower = PurchasingPower.sort_values('Local Purchasing Power Index', ascending = False)
+
+    PP30=PurchasingPower.head(30)
+    PP30['Local Purchasing Power Index']=100*(PP30['Local Purchasing Power Index']/PP30['Local Purchasing Power Index'].mean()-1)
+    colormat2=np.where(PP30['Local Purchasing Power Index']>0, 'g','r')
+    a = PurchasingPower.head(20)['City']
+    b = PurchasingPower.head(20)['Local Purchasing Power Index']
+    plt.title("Purchasing Power")
+    plt.xlabel("% Disparity")
+    plt.ylabel("Cities")
+    plt.barh(a,b,color =colormat2, edgecolor="black")
+    plt.show()
+    plt.close()
+
+def main():
+    sorted_df = mergeData()
+    Cost, PurchasingPower =FixData(sorted_df)
+    Charting(Cost, PurchasingPower)
+main()
